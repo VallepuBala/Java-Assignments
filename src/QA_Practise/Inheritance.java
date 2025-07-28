fb72342e9b97f66333940e977666ee712f5453f1
VallepuBala <baladurgeshv1@gmail.com>
2025-07-28 09:30:04 +0530

    Inheritance

---
+++ src/QA_Practise/Inheritance.java
@@ -0,0 +1,36 @@
+package QA_Practise;
+
+public class Inheritance {
+	String parentName;
+	int parentAge;
+	
+//	public Inheritance(String parentName, int parentAge) {
+//		this.parentName = parentName;
+//		this.parentAge = parentAge;
+//	}
+	
+	public void willDo() {
+		System.out.println("Parent will do work.");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		class Child extends Inheritance{
+			
+			@ Override
+			public void willDo() {
+				System.out.println("Child will play.");
+			}
+		}
+		
+		Child c1 = new Child();
+		c1.willDo();
+		
+	}
+
+}
+
+// Learning: 
+//If we want to create child class outside main(String[] args) then we should create it as static. (static class Child ...)
+// if we create a parameterized constructor in parent then we must call it using super() in child class.
